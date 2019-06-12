e29c9e91d338f5c29248fc4a6267c099ddb6f999
Miguel Araújo <mdevelap@gmail.com>
2019-06-12 19:32:10 +0100

    scale up master script

--- master_scripts/scale-up.sh
+++ master_scripts/scale-up.sh
@@ -1,15 +1,32 @@
-
-
-# docker service scale $1=N
-
 # min_instances_per_service=5
 max_instances_per_service=10
 
 curr_total_intances_1=$(sudo docker service ls | grep yolo_tf_service1 | awk {'print $4'} | cut -d "/" -f 2)
 curr_total_intances_2=$(sudo docker service ls | grep yolo_tf_service2 | awk {'print $4'} | cut -d "/" -f 2)
 
-curr_total_intances=$(($curr_total_intances_1>$curr_total_intances_2?$curr_total_intances_1:$curr_total_intances_2))
+echo "curr_total_intances_1: $curr_total_intances_1"
+echo "curr_total_intances_2: $curr_total_intances_2"
 
-if [[ ( "$curr_total_intances" < $max_instances_per_service ) ]] ; then
-    echo "$curr_total_intances"
+if [ -z "$curr_total_intances_1" ]
+then
+    exit 0
+fi
 
+if (( $curr_total_intances_1 < $curr_total_intances_2 )) ; then
+    min_total_intances=$curr_total_intances_1
+    min_service="yolo_tf_service1"
+else
+    min_total_intances=$curr_total_intances_2
+    min_service="yolo_tf_service2"
+fi
+
+
+echo "min_service: $min_service"
+echo "min_total_intances: $min_total_intances"
+
+desired_instances=$((min_total_intances + 1))
+
+if (( $desired_instances <= $max_instances_per_service )) ; then
+    echo "CAN SCALE UP"
+    sudo docker service scale $min_service=$desired_instances
+fi
